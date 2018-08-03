f9093376d2291ce7345e530d8d1179179d946591
lijiaming <909903535@qq.com>
2018-08-03 19:23:37 +0800

    完成首页头部和底部静态页面

--- src/main.js
+++ src/main.js
@@ -3,11 +3,18 @@ import Vue from 'vue'
 // 引入App组件
 import App from './App.vue'
 
+
+
+// 引入网站的的css
+import css from './assets/statics/site/css/style.css'
+
 // 关闭窗口错误提示
 Vue.config.productionTip = false
 
 // 实例化vue对象
 new Vue({
+  //选择器
   el:'#app',
+  // 渲染App组件
   render: h => h(App)
 })
